4d44f58a38280d3e27e5b9e7fbebb1dd1c0d05b9
Millena Ferreira <millenamills44@gmail.com>
2023-03-12 21:05:26 -0300

    segunda tela em andamento(faltando a ação do click)

--- app/src/main/java/br/senai/sp/jandira/mylogin/SignUpActivity.kt
+++ app/src/main/java/br/senai/sp/jandira/mylogin/SignUpActivity.kt
@@ -3,11 +3,8 @@ package br.senai.sp.jandira.mylogin
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -17,28 +14,26 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import br.senai.sp.jandira.mylogin.ui.theme.MyLoginTheme
-import java.util.*
+import androidx.compose.material.CheckboxColors as CheckboxColors
 
-class MainActivity : ComponentActivity() {
+class SignUpActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyLoginTheme {
-                LoginScreen()
+                LoginRegister()
             }
         }
     }
 }
 
-
 @Preview(showSystemUi = true, showBackground = true)
 @Composable
-fun LoginScreen() {
+fun LoginRegister() {
     Surface(
         modifier = Modifier.fillMaxSize(),
     ) {
@@ -60,39 +55,41 @@ fun LoginScreen() {
                 ) {}
             }
 
-            Spacer(modifier = Modifier.height(140.dp))
+            Spacer(modifier = Modifier.height(10.dp))
 
             //Form
             Column(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(17.dp)
-            ) {
+                    .padding(17.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            )
+            {
                 Text(
-                    text = stringResource(id = R.string.title_login),
-                    fontSize = 40.sp,
+                    text = stringResource(id = R.string.sign_up),
                     color = colorResource(id = R.color.pink_login),
-                    fontWeight = FontWeight.ExtraBold
+                    fontSize = 32.sp,
+                    fontWeight = FontWeight.W700
                 )
                 Text(
-                    text = stringResource(id = R.string.subtitle_login),
+                    text = "Create a new account",
                     color = Color.Gray,
-                    fontSize = 14.sp
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.W400
                 )
 
-                Spacer(modifier = Modifier.height(87.dp))
-
+                Spacer(modifier = Modifier.height(45.dp))
+                
                 OutlinedTextField(
-                    value = "teste@email.com",
+                    value = "Susanna Hoffs",
                     onValueChange = {},
                     modifier = Modifier.fillMaxWidth(),
                     shape = RoundedCornerShape(16.dp),
                     label = {
                         Text(
-                            text = stringResource(id = R.string.email)
+                            text = "username"
                         )
-                            },
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                    },
                     leadingIcon = {
                         Icon(
                             painter = painterResource(
@@ -102,27 +99,24 @@ fun LoginScreen() {
                             tint = colorResource(id = R.color.pink_login)
                         )
                     }
-
-
                 )
 
-                Spacer(modifier = Modifier.height(31.dp))
+                Spacer(modifier = Modifier.height(21.dp))
 
                 OutlinedTextField(
-                    value = "**********",
+                    value = "99999-0987",
                     onValueChange = {},
                     modifier = Modifier.fillMaxWidth(),
                     shape = RoundedCornerShape(16.dp),
                     label = {
                         Text(
-                            text = stringResource(id = R.string.password)
+                            text = "phone"
                         )
-                            },
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                    },
                     leadingIcon = {
                         Icon(
                             painter = painterResource(
-                                id = R.drawable.baseline_lock_24
+                                id = R.drawable.baseline_email_24
                             ),
                             contentDescription = "",
                             tint = colorResource(id = R.color.pink_login)
@@ -130,53 +124,112 @@ fun LoginScreen() {
                     }
                 )
 
-                Spacer(modifier = Modifier.height(31.dp))
-                Column(
+                Spacer(modifier = Modifier.height(21.dp))
+
+                OutlinedTextField(
+                    value = "susanna@email.com ",
+                    onValueChange = {},
                     modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.End
-                ) {
-                    Button(
-                        onClick = {  },
-                        shape = RoundedCornerShape(16.dp),
-                        modifier = Modifier
-                            .height(48.dp)
-                            .width(134.dp),
-                        colors = ButtonDefaults.buttonColors(colorResource(id = R.color.pink_login))
-                    ) {
+                    shape = RoundedCornerShape(16.dp),
+                    label = {
                         Text(
-                            text = stringResource(id = R.string.sign_up).uppercase(Locale.getDefault()),
-                            color = Color.White
+                            text = stringResource(id = R.string.email)
                         )
+                    },
+                    leadingIcon = {
                         Icon(
                             painter = painterResource(
-                                id = R.drawable.baseline_east_24
+                                id = R.drawable.baseline_email_24
                             ),
-                            tint = Color.White,
-                            contentDescription = ""
+                            contentDescription = "",
+                            tint = colorResource(id = R.color.pink_login)
+                        )
+                    }
+                )
+
+                Spacer(modifier = Modifier.height(21.dp))
+
+                OutlinedTextField(
+                    value = "************",
+                    onValueChange = {},
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = RoundedCornerShape(16.dp),
+                    label = {
+                        Text(
+                            text = stringResource(id = R.string.password)
+                        )
+                    },
+                    leadingIcon = {
+                        Icon(
+                            painter = painterResource(
+                                id = R.drawable.baseline_email_24
+                            ),
+                            contentDescription = "",
+                            tint = colorResource(id = R.color.pink_login)
+                        )
+                    }
+                )
+
+                Spacer(modifier = Modifier.height(21.dp))
+                
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.Start
+                ) {
+                    Row() {
+                       Checkbox(
+                           checked = false,
+                           onCheckedChange = {}
+                       )
+                        Row(Modifier.padding(top = 14.dp)) {
+                            Text(text = "Over 18?")
+                        }
+                    }
+
+                }
+
+                Spacer(modifier = Modifier.height(23.dp))
+
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Button(
+                        onClick = { /*TODO*/ },
+                        shape = RoundedCornerShape(16.dp),
+                        colors = ButtonDefaults.buttonColors(colorResource(id = R.color.pink_login)),
+                        modifier = Modifier.fillMaxWidth().height(48.dp)
+                    )
+                    {
+                        Text(
+                            text = "CREATE ACCOUNT",
+                            color = Color.White
                         )
                     }
                 }
+
                 Spacer(modifier = Modifier.height(31.dp))
+
                 Column(
                     modifier = Modifier.fillMaxWidth(),
                     horizontalAlignment = Alignment.End
                 ) {
                     Row() {
                         Text(
-                            text = stringResource(id = R.string.have_account)
+                            text = "Already have an account?"
                         )
                         Row(
                             modifier = Modifier.padding(start = 3.dp)
                         ) {
                             Text(
-                                text = stringResource(id = R.string.sign_up),
+                                text = "Sign in",
                                 color = colorResource(id = R.color.pink_login)
                             )
                         }
                     }
                 }
-
-                }
+                //
+            }
             Row(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.Start
@@ -189,9 +242,7 @@ fun LoginScreen() {
                     shape = RoundedCornerShape(0.dp, 15.dp, 0.dp, 0.dp)
                 ) {}
             }
-
         }
 
     }
-
 }
